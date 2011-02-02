b58fba7755c53298eaf8bf7acdd53d0afe099f89
Nicholas Greenfield <Greenie0506@gmail.com>
2011-02-02 16:15:34 -0500

    Took out Disqus

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Syllabi::Application.initialize!
 
-config.after_initialize do 
-Disqus::defaults[:account] = "Greenie0506" 
-# so that the comments will load up in development environment 
-Disqus::defaults[:developer] = true 
-Disqus::defaults[:container_id] = "disqus_thread" 
-Disqus::defaults[:show_powered_by] = false 
-End 
-
-config.gem 'disqus'
