9b1b63a41718e54ce79132fa232fa90045a872c6
Nicholas Greenfield <Greenie0506@gmail.com>
2011-03-02 19:09:31 -0500

    styling

--- app/models/syllabus.rb
+++ app/models/syllabus.rb
@@ -3,6 +3,5 @@ class Syllabus < ActiveRecord::Base
   validates :professor, :presence => true
   
   has_attached_file :syllabi_upload, :styles => {:small => "150x150>"}
-  
-  
+                                    
 end
